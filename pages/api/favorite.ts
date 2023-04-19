e5366af32e3f0c4da1dbea789d4870d825cb9770
Matias <acostamatias25@gmail.com>
2023-04-19 17:09:24 -0300

    delete: se borro un console.log

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -11,7 +11,6 @@ export default async function handler(
     if (req.method === "POST") {
       const { currentUser } = await serverAuth(req, res);
       const { movieId } = req.body;
-      console.log(movieId);
 
       const existingMovie = await prismadb.movie.findUnique({
         where: {
