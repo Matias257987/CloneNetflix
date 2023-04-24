f32f33c43702571880fc8b9d1ec9f3fea6e41a53
Matias <acostamatias25@gmail.com>
2023-04-24 15:41:04 -0300

    errors

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -65,10 +65,10 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  // jwt: {
-  //   secret: process.env.NEXTAUTH_JWT_SECRET,
-  // },
-  // secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
